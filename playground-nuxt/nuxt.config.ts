af8ae391b18b92e059c4c867efaf7d52538f7a1d
glennmichael123 <gtorregosa@gmail.com>
2025-08-15 20:27:48 +0800

    chore: wip

--- playground-nuxt/nuxt.config.ts
+++ playground-nuxt/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  modules: ['ts-maps-nuxt'],
   devtools: { enabled: true },
 })
