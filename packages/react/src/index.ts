8c164a43677470b226fe9bafa5d733bbd6f705ef
glennmichael123 <gtorregosa@gmail.com>
2025-08-18 18:53:36 +0800

    chore: wip

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,2 +1,8 @@
 export { VectorMap } from './components/VectorMap'
 export type { VectorMapProps } from './components/VectorMap'
+
+export { UnitedStates } from './components/UnitedStates'
+export type { UnitedStatesProps } from './components/UnitedStates'
+
+export { Canada } from './components/Canada'
+export type { CanadaProps } from './components/Canada'
