ba57a22dc5fa4a9efdf37b3ec6185d4e74fbdd53
glennmichael123 <gtorregosa@gmail.com>
2025-08-18 19:01:50 +0800

    chore: wip

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -6,3 +6,21 @@ export type { UnitedStatesProps } from './components/UnitedStates'
 
 export { Canada } from './components/Canada'
 export type { CanadaProps } from './components/Canada'
+
+export { Spain } from './components/Spain'
+export type { SpainProps } from './components/Spain'
+
+export { Iraq } from './components/Iraq'
+export type { IraqProps } from './components/Iraq'
+
+export { Russia } from './components/Russia'
+export type { RussiaProps } from './components/Russia'
+
+export { WorldMap } from './components/WorldMap'
+export type { WorldMapProps } from './components/WorldMap'
+
+export { Italy } from './components/Italy'
+export type { ItalyProps } from './components/Italy'
+
+export { Brasil } from './components/Brasil'
+export type { BrasilProps } from './components/Brasil'
