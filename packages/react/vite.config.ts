5b2930dc5bbb98d2329ed7c06d22871d90f7becb
glennmichael123 <gtorregosa@gmail.com>
2025-10-22 23:29:05 +0800

    chore: wip

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,12 +1,10 @@
 import { resolve } from 'node:path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   plugins: [
     react(),
-    dts({ rollupTypes: true }),
   ],
   build: {
     lib: {
